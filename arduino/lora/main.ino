dfa6dad551ed5b420e35500e022e2d5c1ec19490
Jairo Esteban Martínez Portillo <jairoboss00@gmail.com>
2022-06-05 01:08:36 -0500

    new get request

--- arduino/lora/main.ino
+++ arduino/lora/main.ino
@@ -1,3 +1,13 @@
+#include <SPI.h>
+#include <LoRa.h>
+#define CONFIG_MOSI 27
+#define CONFIG_MISO 19
+#define CONFIG_CLK  5
+#define CONFIG_NSS  18
+#define CONFIG_RST  23
+#define CONFIG_DIO0 26
+#define LORA_PERIOD 433
+#define BAND 433910000
 #include <Wire.h>
 #include "SSD1306Wire.h"
 #define OLED_CLASS_OBJ  SSD1306Wire
@@ -5,47 +15,61 @@
 #define OLED_SDA    21
 #define OLED_SCL    22
 #define OLED_RST    -1
-#define CONFIG_MOSI 27
-#define CONFIG_MISO 19
-#define CONFIG_CLK  5
-#define CONFIG_NSS  18
-#define CONFIG_RST  23
-#define CONFIG_DIO0 26
+#define LED1    25
 #define SDCARD_MOSI -1
 #define SDCARD_MISO -1
 #define SDCARD_SCLK -1
 #define SDCARD_CS   -1
 #include <Arduino.h>
-OLED_CLASS_OBJ tft(OLED_ADDRESS, OLED_SDA, OLED_SCL);
 #include <WiFi.h>
 #include <WiFiMulti.h>
 #include <HTTPClient.h>
 #define USE_SERIAL Serial
 WiFiMulti wifiMulti;
-#include <SPI.h>
+//array of frequencies valid for the application to change
+long int frequencies[3] = {433910000, 315000000, 868000000};
+//controls the current frequency index in the array
+int indexFreq = 0;
+volatile int incomingPacketSize;
+int rssi = 0;
+OLED_CLASS_OBJ tft(OLED_ADDRESS, OLED_SDA, OLED_SCL);
+int cpo = 0;
 int t = 0;
 String tmp2;
 void setup() {
   USE_SERIAL.begin(115200);
+  pinMode(LED1, OUTPUT);
+
+  // Serial.begin(9600);
+  //  while (!Serial);
+  LoRa.setGain(20);
+  SPI.begin(CONFIG_CLK, CONFIG_MISO, CONFIG_MOSI, CONFIG_NSS);
+  LoRa.setPins(CONFIG_NSS, CONFIG_RST, CONFIG_DIO0);
+  LoRa.begin(frequencies[indexFreq]);
+  LoRa.receive();
   tft.init();
   tft.setTextAlignment(TEXT_ALIGN_CENTER);
   tft.flipScreenVertically();
-  tft.clear();
+  tft.clear();    
+  digitalWrite(LED1, HIGH);
   tft.drawString(57, 3, "! Binance x Arduino !");
-  tft.drawString(60, 20, "V1.01");
-  tft.display();
+  tft.drawString(60, 19, "V1.01");  
+  tft.drawLine(5, 32, 128, 30);
+  tft.drawString(57, 34, "Jairo x Daniel");
+  tft.drawLine(5, 50, 130, 50);
+  tft.display();      
+  delay(2000);     
   delay(2000);
-  wifiMulti.addAP("Ponciano", "ladesiemprecrack1");;
+  tft.clear();
+  digitalWrite(LED1, LOW);
+  wifiMulti.addAP("Ponciano", "ladesiemprecrack1");
 }
 
 void loop() {
-  if ((wifiMulti.run() == WL_CONNECTED)) {
+ if ((wifiMulti.run() == WL_CONNECTED)) {
     HTTPClient http;
     USE_SERIAL.print("[HTTP] begin...\n");
-    http.begin("http://192.168.0.4:8080/");
-    //http.begin("https://api.thingspeak.com/apps/thinghttp/send_request?api_key=12BB7OMBEIN3IIJJ");
-    //http.begin ("https://api.thingspeak.com/apps/thinghttp/send_request?api_key=Y8PWPLDACM1J84EQ");
-    // http.begin("https://api.thingspeak.com/apps/thinghttp/send_request?api_key=BZOJWIRDNJ6GAMM6");
+    http.begin("https://microapisoftware.herokuapp.com/api/movimiento/ultimo-movimiento");    
     int httpCode = http.GET();
     String payload = http.getString();
     char buf[1000];
@@ -55,88 +79,11 @@ void loop() {
     tft.clear();
     USE_SERIAL.println(tmp2);
     http.end();
-  }
+  } 
   for (int i = 500; i >= 0; i--) {
     tft.clear();
-    tft.drawString(60, 3, "! NOS NIEUWS !");
+    tft.drawString(60, 3, "Ultimo movimiento");
     tft.drawString(-200 + i, 32, tmp2);
-    tft.drawLine(5, 30, 128, 30);
-    tft.drawLine(5, 50, 128, 50);
-    tft.display();
-    delay(10);
-  }
-  for (int i = 0; i <= 500; i++) {
-    tft.clear();
-    tft.drawString(60, 3, "! NOS NIEUWS !");
-    tft.drawString(-200 - i, 32, tmp2);
-    tft.drawLine(5, 30, 128, 30);
-    tft.drawLine(5, 50, 128, 50);
-    tft.display();
-    delay(10);
-  }
-  tft.setTextAlignment(TEXT_ALIGN_CENTER);
-  ///////////////////////////////////////////////////////////////////////////
-  if ((wifiMulti.run() == WL_CONNECTED)) {
-    HTTPClient http;
-    USE_SERIAL.print("[HTTP] begin...\n");
-    http.begin("https://api.thingspeak.com/apps/thinghttp/send_request?api_key=12BB7OMBEIN3IIJJ");
-    int httpCode = http.GET();
-    String payload = http.getString();
-    char buf[1000];
-    payload.toCharArray(buf, 1000);
-    tmp2 = buf ;
-    tmp2.trim();
-    tft.clear();
-    USE_SERIAL.println(tmp2);
-    http.end();
-  }
-  for (int i = 500; i >= 0; i--) {
-    tft.clear();
-    tft.drawString(60, 3, "! P2000 !");
-    tft.drawString(-200 + i, 32, tmp2);
-    tft.drawLine(5, 30, 128, 30);
-    tft.drawLine(5, 50, 128, 50);
-    tft.display();
-    delay(10);
-  }
-  for (int i = 0; i <= 500; i++) {
-    tft.clear();
-    tft.drawString(60, 3, "! P2000 !");
-    tft.drawString(-200 - i, 32, tmp2);
-    tft.drawLine(5, 30, 128, 30);
-    tft.drawLine(5, 50, 128, 50);
-    tft.display();
-    delay(10);
-  }
-  tft.setTextAlignment(TEXT_ALIGN_CENTER);
-  //////////////////////////////////////////////////////////////
-  if ((wifiMulti.run() == WL_CONNECTED)) {
-    HTTPClient http;
-    USE_SERIAL.print("[HTTP] begin...\n");
-    http.begin("https://api.thingspeak.com/apps/thinghttp/send_request?api_key=BZOJWIRDNJ6GAMM6");
-    int httpCode = http.GET();
-    String payload = http.getString();
-    char buf[1000];
-    payload.toCharArray(buf, 1000);
-    tmp2 = buf ;
-    tmp2.trim();
-    tft.clear();
-    USE_SERIAL.println(tmp2);
-    http.end();
-  }
-  for (int i = 500; i >= 0; i--) {
-    tft.clear();
-    tft.drawString(60, 3, "! De Tijdt !");
-    tft.drawString(-200 + i, 32, tmp2);
-    tft.drawLine(5, 30, 128, 30);
-    tft.drawLine(5, 50, 128, 50);
-    tft.display();
-    delay(10);
-  }
-  for (int i = 0; i <= 500; i++) {
-    tft.clear();
-    tft.drawString(60, 3, "! De Tijdt !");
-    tft.drawString(-200 - i, 32, tmp2);
     tft.drawLine(5, 30, 128, 30);
     tft.drawLine(5, 50, 128, 50);
     tft.display();
@@ -146,7 +93,7 @@ void loop() {
    if ((wifiMulti.run() == WL_CONNECTED)) {
     HTTPClient http;
     USE_SERIAL.print("[HTTP] begin...\n");
-    http.begin("https://api.thingspeak.com/apps/thinghttp/send_request?api_key=DXLRW4WOOIRQWZSM");
+    http.begin("https://microapisoftware.herokuapp.com/api/movimiento/ultimo-movimiento");
     int httpCode = http.GET();
     String payload = http.getString();
     char buf[1000];
@@ -156,24 +103,88 @@ void loop() {
     tft.clear();
     USE_SERIAL.println(tmp2);
     http.end();
-  }
-  for (int i = 1000; i >= 0; i--) {
+  }  
+  tft.setTextAlignment(TEXT_ALIGN_CENTER);
+
+
+if ((wifiMulti.run() == WL_CONNECTED)) {
+    HTTPClient http;
+    USE_SERIAL.print("[HTTP] begin...\n");
+    http.begin("https://microapisoftware.herokuapp.com/api/binance/btc-usdt");    
+    int httpCode = http.GET();
+    String payload = http.getString();
+    char buf[1000];
+    payload.toCharArray(buf, 1000);
+    tmp2 = buf ;
+    tmp2.trim();
     tft.clear();
-    tft.drawString(60, 3, "! Het Weer !");
+    USE_SERIAL.println(tmp2);
+    http.end();
+  } 
+  for (int i = 500; i >= 0; i--) {
+    tft.clear();
+    tft.drawString(60, 3, "Precio Bitcoin");
     tft.drawString(-200 + i, 32, tmp2);
     tft.drawLine(5, 30, 128, 30);
     tft.drawLine(5, 50, 128, 50);
     tft.display();
     delay(10);
   }
-  for (int i = 0; i <= 1000; i++) {
+  tft.setTextAlignment(TEXT_ALIGN_CENTER);
+   if ((wifiMulti.run() == WL_CONNECTED)) {
+    HTTPClient http;
+    USE_SERIAL.print("[HTTP] begin...\n");
+    http.begin("https://microapisoftware.herokuapp.com/api/binance/btc-usdt");
+    int httpCode = http.GET();
+    String payload = http.getString();
+    char buf[1000];
+    payload.toCharArray(buf, 1000);
+    tmp2 = buf ;
+    tmp2.trim();
     tft.clear();
-    tft.drawString(60, 3, "! Het Weer !");
-    tft.drawString(-200 - i, 32, tmp2);
+    USE_SERIAL.println(tmp2);
+    http.end();
+  }  
+  tft.setTextAlignment(TEXT_ALIGN_CENTER);
+
+
+  if ((wifiMulti.run() == WL_CONNECTED)) {
+    HTTPClient http;
+    USE_SERIAL.print("[HTTP] begin...\n");
+    http.begin("https://microapisoftware.herokuapp.com/api/binance/eth-usdt");    
+    int httpCode = http.GET();
+    String payload = http.getString();
+    char buf[1000];
+    payload.toCharArray(buf, 1000);
+    tmp2 = buf ;
+    tmp2.trim();
+    tft.clear();
+    USE_SERIAL.println(tmp2);
+    http.end();
+  } 
+  for (int i = 500; i >= 0; i--) {
+    tft.clear();
+    tft.drawString(60, 3, "Precio Ethereum");
+    tft.drawString(-200 + i, 32, tmp2);
     tft.drawLine(5, 30, 128, 30);
     tft.drawLine(5, 50, 128, 50);
     tft.display();
     delay(10);
   }
+  tft.setTextAlignment(TEXT_ALIGN_CENTER);
+   if ((wifiMulti.run() == WL_CONNECTED)) {
+    HTTPClient http;
+    USE_SERIAL.print("[HTTP] begin...\n");
+    http.begin("https://microapisoftware.herokuapp.com/api/binance/eth-usdt");
+    int httpCode = http.GET();
+    String payload = http.getString();
+    char buf[1000];
+    payload.toCharArray(buf, 1000);
+    tmp2 = buf ;
+    tmp2.trim();
+    tft.clear();
+    USE_SERIAL.println(tmp2);
+    http.end();
+  }  
   tft.setTextAlignment(TEXT_ALIGN_CENTER);
 }
